36ded78460f1babda5f1e9284d91378e567ea779
gustonecrush <codeboxezz@gmail.com>
2023-01-24 00:29:44 +0700

    edit tailwindcss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,14 @@ module.exports = {
       fontFamily: {
         poppins: ["Poppins", "sans-serif"],
       },
-      colors: {},
+      colors: {
+        primary: "#0A9F82",
+        secondary: "#101939",
+        background: "#EFF3F9",
+        textPrimary: "#020B2A",
+        textSecondary: "#A7A9BE",
+        textTersier: "#A9ABBF",
+      },
     },
   },
   plugins: [],

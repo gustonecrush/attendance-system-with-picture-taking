a97b76bc7dd3502cf97ff1e57ee7f7badee3dbae
gustonecrush <codeboxezz@gmail.com>
2023-01-24 16:27:30 +0700

    created time clock

--- src/components/Absent.js
+++ src/components/Absent.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Heading } from "./microcomponents";
 
 function Absent({ type }) {
+  const [date, setDate] = useState(new Date());
+
+  const tick = () => {
+    setDate(new Date());
+  };
+
+  useEffect(() => {
+    var timerID = setInterval(() => tick(), 1000);
+
+    return function cleanup() {
+      clearInterval(timerID);
+    };
+  }, []);
+
   return (
     <div>
       <div>
-        <Heading title={type} />
+        <div className="w-full mt-7 p-8 bg-white max-h-screen min-h-fit overflow-scroll rounded-2xl shadow-aestheticShadow">
+          <h1 className="text-secondary font-light text-[16px]">
+            {`${date.toLocaleTimeString()}`}
+          </h1>
+          <Heading title={type} />
+        </div>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,8 @@ module.exports = {
         poppins: ["Poppins", "sans-serif"],
       },
       boxShadow: {
-        primaryShadow: '0 35px 60px -15px rgba(10, 159, 130, 0.2)',
+        aestheticShadow: "rgba(149, 157, 165, 0.2) 0px 8px 24px;",
+        primaryShadow: "0 35px 60px -15px rgba(10, 159, 130, 0.2)",
       },
       colors: {
         primary: "#0A9F82",
